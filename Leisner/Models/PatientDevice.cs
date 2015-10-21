4746470f43fae2f071a6272fadb6b10590a27725
nickosv <niickj@live.dk>
2015-10-21 11:50:50 +0200

    Models FiNaL

---
+++ Leisner/Models/PatientDevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Leisner.Models {
+    public class PatientDevice {
+        public int ID { get; set; }
+        public DateTime HandOutDate { get; set; }
+        public DateTime HandInDate { get; set; }
+        public Patient Patient { get; set; }
+        public Device Device { get; set; }
+        public List<Measurement> Measurements { get; set; }
+    }
+}
