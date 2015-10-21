84d9a0b3c0c102a9e5e370214edbece4cd15d39c
Peter Bork <peter-b-n@hotmail.com>
2015-10-21 17:04:07 +0200

    Get & Post working

--- Leisner/Controllers/PatientController.cs
+++ Leisner/Controllers/PatientController.cs
@@ -9,38 +9,43 @@ namespace WebAPI.Controllers{
     public class PatientController : ApiController {
 
         List<Models.PatientDevice> patientDeviceList = new List<Models.PatientDevice>();
-        List<Models.Patient> patientList = new List<Models.Patient>();
+        static List<Models.Patient> patientList = new List<Models.Patient>() { new Models.Patient(0, "Nick", 22, "male") , new Models.Patient(1, "Jonas", 22, "male") };
         List<Models.Device> deviceList = new List<Models.Device>();
         
         
 
         [HttpPost]
-        public void RegisterPatient(string name, int age, string sex) {
-            int id = patientList.Count;
+        // Post
+        // Content-Type: application/json
+        // Request body: {"ID":2,"Name":"peter","Age":22,"Sex":"male"
+        public void RegisterPatient([FromBody]Models.Patient patient) {
+            //int id = patientList.Count;
 
-            Models.Patient patient = new Models.Patient(id, name, age, sex);
+            Models.Patient _patient = new Models.Patient(patient.ID, patient.Name, patient.Age, patient.Sex);
 
-            patientList.Add(patient);
+            patientList.Add(_patient);
         }
 
         [HttpGet]
-        public List<Models.Patient> Get() {
+        // Get
+        // api/Patient
+        public List<Models.Patient> GetPatients() {
             return patientList;
         }
 
-        [HttpPost]
-        public void HandOutDevice(Models.Patient patient, Models.Device device) {
-            Models.PatientDevice patientDevice = new Models.PatientDevice();
-            patientDevice.HandOutDate = DateTime.Now;
-            patientDevice.Patient = patient;
+        //[HttpPost]
+        //public void HandOutDevice(Models.Patient patient, Models.Device device) {
+        //    Models.PatientDevice patientDevice = new Models.PatientDevice();
+        //    patientDevice.HandOutDate = DateTime.Now;
+        //    patientDevice.Patient = patient;
 
-            patientDeviceList.Add(patientDevice);
-        }
+        //    patientDeviceList.Add(patientDevice);
+        //}
 
-        [HttpPost]
-        public void HandInDevice(Models.PatientDevice patientDevice) { 
-            patientDevice.HandInDate = DateTime.Now;
-        }
+        //[HttpPost]
+        //public void HandInDevice(Models.PatientDevice patientDevice) { 
+        //    patientDevice.HandInDate = DateTime.Now;
+        //}
 
         [HttpGet]
         public List<Models.Measurement> SearchMeasurements(string searchType, string searchValue) {
